33f028e9d75ecc5da0eeb93cac9d752695cc3fbb
A Tillman <outpost@mac.lan>
2025-08-18 07:04:18 -0600

    Dev and production dbs

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@
  * This file is generated/updated by the toolbench database installation process
  */
 
-export const bizDataConfig = {
+export const bizDataConfigDev = {
   "apiKey": "AIzaSyAEB5jxCyFQTfuK-_ML2pl8MUdtC317Ql8",
   "authDomain": "biz-data-a1535.firebaseapp.com",
   "projectId": "biz-data-a1535",
@@ -12,7 +12,7 @@ export const bizDataConfig = {
   "appId": "1:860715627444:web:5bb0171c631a950f1538e1"
 };
 
-export const bizTrendsConfig = {
+export const bizTrendsConfigDev = {
   "apiKey": "AIzaSyAro96U0oHLf4sBSDqfp7fCbrfoEgSrqP0",
   "authDomain": "biz-trends.firebaseapp.com",
   "projectId": "biz-trends",
@@ -21,7 +21,7 @@ export const bizTrendsConfig = {
   "appId": "1:169753793792:web:c2677362557b5f4e2e4fe5"
 };
 
-export const operationalConfig = {
+export const operationalConfigDev = {
   "apiKey": "AIzaSyAeeF806ooAN1f6vM29ejkf-uPSd63W7gg",
   "authDomain": "operational-5e56f.firebaseapp.com",
   "projectId": "operational-5e56f",
@@ -30,12 +30,48 @@ export const operationalConfig = {
   "appId": "1:1025959230629:web:febce12a315c74e001214c"
 };
 
-export const auditConfig = {
+export const auditConfigDev = {
   "apiKey": "AIzaSyBGtZ0n9IxCuj955UaqX3dm7pNGJ50gguY",
   "authDomain": "audit-88ce6.firebaseapp.com",
   "projectId": "audit-88ce6",
   "storageBucket": "audit-88ce6.firebasestorage.app",
   "messagingSenderId": "313916777937",
   "appId": "1:313916777937:web:456be022ca3d3c5e69ecf0"
+};
+
+export const bizDataConfig = {
+  "apiKey": "AIzaSyCzGYjPEib2WVzM0MrrKXlpaLtGM1A_HYg",
+  "authDomain": "biz-data-94d94.firebaseapp.com",
+  "projectId": "biz-data-94d94",
+  "storageBucket": "biz-data-94d94.firebasestorage.app",
+  "messagingSenderId": "440984472101",
+  "appId": "1:440984472101:web:87b342d49a5ee789918dcc"
+};
+
+export const bizTrendsConfig = {
+  "apiKey": "AIzaSyD4576PGoEAZ7BqZqWHEhCzQt19quC-hVw",
+  "authDomain": "biz-trends-78a0d.firebaseapp.com",
+  "projectId": "biz-trends-78a0d",
+  "storageBucket": "biz-trends-78a0d.firebasestorage.app",
+  "messagingSenderId": "402111023147",
+  "appId": "1:402111023147:web:7d1899abe30519c128026e"
+};
+
+export const operationalConfig = {
+  "apiKey": "AIzaSyAxRUiwudb6I5pR66d_BnA2CjYVETB6FnA",
+  "authDomain": "operational-70936.firebaseapp.com",
+  "projectId": "operational-70936",
+  "storageBucket": "operational-70936.firebasestorage.app",
+  "messagingSenderId": "615213444303",
+  "appId": "1:615213444303:web:cefa9a8c18e4a16b631bba"
+};
+
+export const auditConfig = {
+  "apiKey": "AIzaSyBnwWtveZ2h3dpIDO-lXmqe5gvpuofzytk",
+  "authDomain": "audit-1dab5.firebaseapp.com",
+  "projectId": "audit-1dab5",
+  "storageBucket": "audit-1dab5.firebasestorage.app",
+  "messagingSenderId": "682534284748",
+  "appId": "1:682534284748:web:4fe266a623fe842917c169"
 };
 
